cfa8ab0336a4f31cb5f5160f9b4be62aa0e38a19
Kinetic64 <ben.ward@simplygreat.education>
2026-09-22 12:00:47 +0100

    Added powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool hasPowerup = false;
+    public float playerSpeed = 150f;
     private InputSystem_Actions controls;
     private Rigidbody playerRb;
     private GameObject focalPoint;
-    public float playerSpeed = 150f;
+    private float powerupStrength = 15.0f;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -26,4 +30,25 @@ public class PlayerController : MonoBehaviour
         float forwardInput = moveInput.y;
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * playerSpeed * Time.deltaTime);
     }    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy (other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbogy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
+            enemyRigidbogy.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
 }
